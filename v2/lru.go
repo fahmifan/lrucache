a0c52497da3b8b749544dad3c0d71d0f663c47c6
fahmifan <me@fahmifan.id>
2021-10-31 12:08:08 +0700

    feature: add auto eviction

--- v2/lru.go
+++ v2/lru.go
@@ -2,7 +2,9 @@ package lrucache
 
 import (
 	"container/list"
+	"log"
 	"sync"
+	"time"
 )
 
 // Item ..
@@ -11,14 +13,24 @@ type Item struct {
 	key     string
 	value   interface{}
 	element *list.Element
+	// unix timestamp in second, <= 0 means no expiry
+	expireAt int64
 }
 
 // NewItem ..
-func NewItem(key string, val interface{}) *Item {
+func NewItem(key string, val interface{}, expireAt int64) *Item {
 	return &Item{
-		key:   key,
-		value: val,
+		key:      key,
+		value:    val,
+		expireAt: expireAt,
 	}
+}
+
+func (n *Item) CheckExpire(now int64) bool {
+	if n.expireAt == 0 {
+		return false
+	}
+	return now > n.expireAt
 }
 
 func (n *Item) SetElement(el *list.Element) {
@@ -36,9 +48,24 @@ func (n *Item) SetValue(v interface{}) {
 // DefaultMaxSize ..
 const DefaultMaxSize int64 = 24
 
+type Option func(l *LRUCacher)
+
+func WithOnEvicted(onEvicted func(key string, val interface{})) Option {
+	return func(l *LRUCacher) {
+		l.OnEvicted = onEvicted
+	}
+}
+
+func WithAutoEviction(enable bool) Option {
+	return func(l *LRUCacher) {
+		l.enableAutoEviction = enable
+	}
+}
+
 // LRUCacher ..
 type LRUCacher struct {
-	maxSize int64
+	maxSize            int64
+	enableAutoEviction bool
 
 	list       *List
 	count      int64
@@ -46,17 +73,26 @@ type LRUCacher struct {
 
 	hash      map[string]*Item
 	hashMutex sync.RWMutex
+
+	OnEvicted func(key string, val interface{})
 }
 
-func NewLRUCacher(maxSize int64) *LRUCacher {
+func NewLRUCacher(maxSize int64, opts ...Option) *LRUCacher {
 	if maxSize < 1 {
 		maxSize = DefaultMaxSize
 	}
-	return &LRUCacher{
+	lru := &LRUCacher{
 		list:    NewList(),
 		hash:    make(map[string]*Item),
 		maxSize: maxSize,
 	}
+	for _, opt := range opts {
+		opt(lru)
+	}
+	if lru.enableAutoEviction {
+		go lru.runJanitor()
+	}
+	return lru
 }
 
 func (l *LRUCacher) getItem(key string) *Item {
@@ -86,8 +122,22 @@ func (l *LRUCacher) queueIsFull() bool {
 	return count == l.maxSize
 }
 
+type putOption struct {
+	expireIn time.Duration // second
+}
+
+type PutOption func(o *putOption)
+
+func WithExpireIn(expireIn time.Duration) PutOption {
+	return func(o *putOption) {
+		if o != nil {
+			o.expireIn = expireIn
+		}
+	}
+}
+
 // Put ..
-func (l *LRUCacher) Put(key string, value interface{}) {
+func (l *LRUCacher) Put(key string, value interface{}, opts ...PutOption) {
 	// if key already exist just replace the cache item
 	oldItem := l.getItem(key)
 	if oldItem != nil {
@@ -95,7 +145,16 @@ func (l *LRUCacher) Put(key string, value interface{}) {
 		return
 	}
 
-	item := NewItem(key, value)
+	option := &putOption{}
+	for _, opt := range opts {
+		opt(option)
+	}
+
+	var expireAt int64
+	if option.expireIn > 0 {
+		expireAt = time.Now().Add(option.expireIn).Unix()
+	}
+	item := NewItem(key, value, expireAt)
 	if l.queueIsFull() {
 		last := l.list.RemoveBack()
 		if last == nil {
@@ -126,6 +185,13 @@ func (l *LRUCacher) Get(key string) interface{} {
 		return nil
 	}
 
+	if l.enableAutoEviction {
+		now := time.Now().Unix()
+		if item.CheckExpire(int64(now)) {
+			return nil
+		}
+	}
+
 	l.list.MoveToFront(item)
 
 	return item.value
@@ -144,6 +210,13 @@ func (l *LRUCacher) Del(key string) interface{} {
 	return node.value
 }
 
+func (l *LRUCacher) Count() int64 {
+	l.countMutex.RLock()
+	count := l.count
+	l.countMutex.RUnlock()
+	return count
+}
+
 func (l *LRUCacher) decCount() {
 	l.countMutex.Lock()
 	l.count--
@@ -154,4 +227,31 @@ func (l *LRUCacher) incCount() {
 	l.countMutex.Lock()
 	l.count++
 	l.countMutex.Unlock()
+}
+
+func (l *LRUCacher) runJanitor() {
+	for {
+		time.Sleep(2 * time.Second)
+		tnow := time.Now()
+		log.Println("janitor runs", tnow)
+
+		l.hashMutex.RLock()
+		var keysToEvict []string
+		now := time.Now().Unix()
+		for key, val := range l.hash {
+			if val.CheckExpire(now) {
+				keysToEvict = append(keysToEvict, key)
+			}
+		}
+		l.hashMutex.RUnlock()
+
+		for _, key := range keysToEvict {
+			val := l.Del(key)
+			if l.OnEvicted != nil {
+				l.OnEvicted(key, val)
+			}
+		}
+
+		log.Printf("janitor finished in %s\n", time.Since(tnow))
+	}
 }
